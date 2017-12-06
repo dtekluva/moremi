21c1562b4866a48482a3b9dcb78f23d9590c4fa1
dtekluva <31896598+dtekluva@users.noreply.github.com>
2017-12-06 15:08:13 +0100

    kkkkk

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,7 +5,10 @@
   -------------------------------------------------------------------------------*/
 
   $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+    $('.preloader').fadeOut(3000); // set duration in brackets    
+  });
+  $(window).load(function(){
+    $('.body').fadeIn(3000); // set duration in brackets    
   });
 
 
@@ -14,25 +17,28 @@
   -------------------------------------------------------------------------------*/
 
     function initParallax() {
-    $('#home').parallax("50%", 0.3);
+    $('.yellow_bar').parallax("50%", 0.3);
 
   }
   initParallax();
 
 
-  /* Back top
+  /* scroll within page
   -----------------------------------------------*/
   
   $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
-        $('.go-top').fadeIn(200);
-        } else {
-          $('.go-top').fadeOut(200);
-        }
-        });   
-        // Animate the scroll to top
-      $('.go-top').click(function(event) {
-        event.preventDefault();
-      $('html, body').animate({scrollTop: 0}, 300);
-      })
-
+    if ($(this).scrollTop() > 200) {
+    $('.go-top').fadeIn(200);
+    } else {
+      $('.go-top').fadeOut(200);
+    }
+    });   
+    // Animate the scroll to top
+  $('.about').click(function(event) {
+    event.preventDefault();
+  $('html, body').animate({scrollTop: 1300}, 1000);
+  })
+  $('.contact').click(function(event) {
+    event.preventDefault();
+  $('html, body').animate({scrollTop: 1300}, 1000);
+  })
